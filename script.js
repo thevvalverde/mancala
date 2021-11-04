b850967248831022f36b92071aa36e637acca4b4
thevvalverde <fvalverde007@gmail.com>
2021-11-04 11:58:17 +0000

    Updated some tags

--- script.js
+++ script.js
@@ -7,16 +7,29 @@ function display() {
         for(i = 0; i < options.length; i++){
             options[i].classList.remove("hidden");
         }
-        document.getElementById("Jogador2").value = "AI Player";
-        document.getElementById("Jogador2").readOnly = true;
+        document.getElementById("player2").value = "AI Player";
+        document.getElementById("player2").readOnly = true;
     }
     else
     {
         for(i = 0; i < options.length; i++){
             options[i].classList.add("hidden");
         }
-        document.getElementById("Jogador2").value = "";
-        document.getElementById("Jogador2").readOnly = false;
+        document.getElementById("player2").value = "";
+        document.getElementById("player2").readOnly = false;
     }
+
+}
+
+function startGame() {
+    let homescreen = document.getElementById("div-homescreen");
+    let gameboard  = document.getElementById("div-gameboard");
+
+    homescreen.classList.toggle("hidden");
+    gameboard.classList.toggle("hidden");
+
+    let cavityNumber = parseInt(document.querySelector('input[name="cavity-number"]:checked').value);
+    let seedNumber   = parseInt(document.getElementById("seednum").value);
+
 
 }
