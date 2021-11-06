ffbaa48e6ea4237f8cbbf72a81d6cceaaf0e0570
Miguel Gonçalves up201905873 <up201905873@up.pt>
2021-11-06 20:17:39 +0000

    Add fade, Player Names and Tutorial Button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function display() {
     let checkbox = document.getElementById("pc-checkbox");
     let options = document.getElementsByClassName("pc-options");
@@ -7,34 +11,80 @@ function display() {
         for(i = 0; i < options.length; i++){
             options[i].classList.remove("hidden");
         }
-        document.getElementById("player2").value = "AI Player";
-        document.getElementById("player2").readOnly = true;
+        document.getElementById("player2Input").value = "AI Player";
+        document.getElementById("player2Input").readOnly = true;
     }
     else
     {
         for(i = 0; i < options.length; i++){
             options[i].classList.add("hidden");
         }
-        document.getElementById("player2").value = "";
-        document.getElementById("player2").readOnly = false;
+        document.getElementById("player2Input").value = "";
+        document.getElementById("player2Input").readOnly = false;
+        document.getElementById("player2Name").innerHTML = "Player 2";
     }
 
 }
 
-function toggleBoard() {
+async function toggleBoard(toggleMode) {
     let homescreen = document.getElementById("div-homescreen");
     let gameboard  = document.getElementById("div-gameboard");
 
-    homescreen.classList.toggle("hidden");
-    gameboard.classList.toggle("hidden");
+
+    if(toggleMode)
+    {
+      fade(homescreen);
+      await sleep(500);
+      homescreen.classList.toggle("hidden");
+      gameboard.classList.toggle("hidden");
+      unfade(gameboard);
+
+    }
+    else
+    {
+      fade(gameboard);
+      await sleep(500);
+      gameboard.classList.toggle("hidden");
+      homescreen.classList.toggle("hidden");
+      unfade(homescreen);
+    }
 
 }
 
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+function unfade(element) {
+    var op = 0.1;
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
 function startGame() {
-    toggleBoard();
- 
+    var toggleMode = true;
+    toggleBoard(toggleMode);
+
     let cavityNumber = parseInt(document.querySelector('input[name="cavity-number"]:checked').value);
     let seedNumber   = parseInt(document.getElementById("seednum").value);
+    let player1 = document.getElementById("player1Input").value;
+    let player2 = document.getElementById("player2Input").value;
 
     let upperRow = document.getElementById("upper-row");
     let lowerRow = document.getElementById("lower-row");
@@ -50,11 +100,20 @@ function startGame() {
         lowerRow.appendChild(newHole);
     }
 
+    if(player1 != "")
+    {
+      document.getElementById("player1Name").innerHTML = player1;
+    }
+    if(player2 != "")
+    {
+      document.getElementById("player2Name").innerHTML = player2;
+    }
 }
 
 
 function quitGame() {
-    toggleBoard();
+    var toggleMode = false;
+    toggleBoard(toggleMode);
 
     let upperRow = document.getElementById("upper-row");
     let child = upperRow.lastElementChild;
